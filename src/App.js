e458af79b5c8cb9fc5d9351ce69dff2abc5e6769
MVM <MVM@d.c>
2023-10-09 16:13:54 +0300

    loadings, missings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {Trans, useTranslation} from "react-i18next";
+
+const lngs = {
+  en: {nativeName: 'English'},
+  de: {nativeName: 'Deutsch'},
+}
 
 function App() {
+  const { t, i18n } = useTranslation()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {Object.keys(lngs).map((lng) => {
+            return (
+                <button type={"submit"} key={lng} onClick={() => i18n.changeLanguage(lng)} disabled={i18n.resolvedLanguage === lng}>
+                  {lngs[lng].nativeName}
+                </button>
+                )
+          })}
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Trans i18nKey='description'>
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
         </p>
         <a
           className="App-link"
@@ -15,8 +33,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('learn')}
         </a>
+        <p>{t('new.key', 'this will be added automatically')}</p>
       </header>
     </div>
   );
